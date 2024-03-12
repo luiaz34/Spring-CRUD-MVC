41525ff314e957b3b133c1a44e869b0ce36ae880
khaing myal htike <khaingmyalhtike3400@gmail.com>
2024-03-12 11:13:56 +0630

    adding the authorization feature to update and delete function

--- src/main/java/khaing/thymeleaf/controller/TestingController.java
+++ src/main/java/khaing/thymeleaf/controller/TestingController.java
@@ -19,18 +19,19 @@ import khaing.thymeleaf.service.EmployeeRestId;
 @Controller
 @RequestMapping("/api")
 public class TestingController {
-    
+
     private EmployeeRepo employeeRepo;
     private EmployeeRestId employeeRestId;
 
     @Autowired
-    public TestingController(EmployeeRepo thEmployeeRepo,EmployeeRestId theEmployeeRestId){
+    public TestingController(EmployeeRepo thEmployeeRepo, EmployeeRestId theEmployeeRestId) {
         this.employeeRepo = thEmployeeRepo;
         this.employeeRestId = theEmployeeRestId;
 
     }
+
     @GetMapping("/list")
-    public String listEmployees(Model theModel){
+    public String listEmployees(Model theModel) {
         employeeRestId.resetEmployeeId();
         List<EmployeeEntity> theEmployees = employeeRepo.findAll();
         theModel.addAttribute("EmployeesList", theEmployees);
@@ -38,33 +39,41 @@ public class TestingController {
     }
 
     @GetMapping("/add")
-    public String add (Model theModel){
+    public String add(Model theModel) {
         EmployeeEntity employeeEntity = new EmployeeEntity();
         theModel.addAttribute("employee", employeeEntity);
         return "employees/createForm";
-        
+
     }
 
     @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("employee") EmployeeEntity employeeEntity){
+    public String saveEmployee(@ModelAttribute("employee") EmployeeEntity employeeEntity) {
         employeeRepo.save(employeeEntity);
         return "redirect:/api/list";
     }
-    
+
     @GetMapping("/update")
-        public String updateEmployee(@RequestParam("employeeId")int theId,Model model){
-            Optional<EmployeeEntity> employeeEntity = employeeRepo.findById(theId);
-            model.addAttribute("employee", employeeEntity);
-            return "employees/updateForm";
-        }
+    public String updateEmployee(@RequestParam("employeeId") int theId, Model model) {
+        Optional<EmployeeEntity> employeeEntity = employeeRepo.findById(theId);
+        model.addAttribute("employee", employeeEntity);
+        return "employees/updateForm";
+    }
+
     @GetMapping("/delete")
-    public String deleteEmplyoee(@RequestParam("employeeId") int theId){
+    public String deleteEmplyoee(@RequestParam("employeeId") int theId) {
         employeeRepo.deleteById(theId);
         return "redirect:/api/list";
 
     }
+
     @GetMapping("/showLogin")
-    public String loginPage(){
-        return "loginRegisterTemplate";
+    public String loginPage() {
+        return "loginRegister/loginRegisterTemplate";
     }
-}   
+
+    @GetMapping("/roleAccessDenied")
+    public String accessDeniedPage() {
+        return "exceptionHandlerTemplate/accessDeniedPage";
+    }
+
+}
--- src/main/java/khaing/thymeleaf/security/EmployeeSecurity.java
+++ src/main/java/khaing/thymeleaf/security/EmployeeSecurity.java
@@ -14,57 +14,57 @@ import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @Configuration
 public class EmployeeSecurity {
     // @Bean
     // public InMemoryUserDetailsManager userDetailsManager(){
-    //     UserDetails john = User.builder()
-    //         .username("john")
-    //         .password("{noop}123")
-    //         .roles("EMPLOYEE")
-    //         .build();
+    // UserDetails john = User.builder()
+    // .username("john")
+    // .password("{noop}123")
+    // .roles("EMPLOYEE")
+    // .build();
 
-    //     UserDetails may=User.builder()
-    //         .username("may")
-    //         .password("{noop}123")
-    //         .roles("EMPLOYEE","MANAGER")
-    //         .build();
-        
-    //     UserDetails jerry=User.builder()
-    //         .username("jerry")
-    //         .password("{noop}123")
-    //         .roles("EMPLOYEE","MANAGER","ADMIN")
-    //         .build();
+    // UserDetails may=User.builder()
+    // .username("may")
+    // .password("{noop}123")
+    // .roles("EMPLOYEE","MANAGER")
+    // .build();
 
+    // UserDetails jerry=User.builder()
+    // .username("jerry")
+    // .password("{noop}123")
+    // .roles("EMPLOYEE","MANAGER","ADMIN")
+    // .build();
 
-    //     return new InMemoryUserDetailsManager(john, may, jerry);
+    // return new InMemoryUserDetailsManager(john, may, jerry);
     // }
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
         return new JdbcUserDetailsManager(dataSource);
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(configurer ->
-                configurer
-                            .requestMatchers("/css/loginRegisterForm.css", "/js/script.js").permitAll()
-                            .anyRequest().authenticated()
-            )
-                .formLogin(form -> 
-                        form
-                            .loginPage("/api/showLogin")
-                            .loginProcessingUrl("/authenticateTheUser")
-                            .permitAll()
-                            .defaultSuccessUrl("/api/list", true)
-
-
-                );
-                return http.build();
-        
+                .authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/css/loginRegisterForm.css", "/js/script.js").permitAll()
+                        .requestMatchers("/api/update").hasAnyRole("MANAGER", "ADMIN")
+                        .requestMatchers("/api/delete").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .formLogin(form -> form
+                        .loginPage("/api/showLogin")
+                        .loginProcessingUrl("/authenticateTheUser")
+                        .permitAll()
+                        .defaultSuccessUrl("/api/list", true))
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // Custom logout URL to match your form action.
+                        .logoutSuccessUrl("/api/showLogin") // Redirect to login page after logout.
+                        .invalidateHttpSession(true) // Invalidate session.
+                        .clearAuthentication(true) // Clear authentication attributes.
+                        .permitAll())
+                .exceptionHandling(configurer -> configurer
+                        .accessDeniedPage("/api/roleAccessDenied"));
+        return http.build();
     }
 
-
 }
- 
